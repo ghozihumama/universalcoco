0e562db04c5e6e86a3e9d832b09a4aa8ebbde43b
maulanaghozi <maulana.ghozi@qbit.co.id>
2022-07-27 16:27:31 +0700

    import asset download

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -24,6 +24,30 @@ import ImgStella from "./stella.png";
 import ImgStellaOff from "./stella-off.png";
 import ImgArya from "./arya.jpeg";
 
+//DesiccatedCoconut
+import ImgDesiccatedCoconut1 from "./DesiccatedCoconut/desiccated-coconut1.png";
+import ImgDesiccatedCoconut2 from "./DesiccatedCoconut/desiccated-coconut2.png";
+import ImgDesiccatedCoconut3 from "./DesiccatedCoconut/desiccated-coconut3.png";
+import ImgDesiccatedCoconut4 from "./DesiccatedCoconut/desiccated-coconut4.png";
+import ImgDesiccatedCoconut5 from "./DesiccatedCoconut/desiccated-coconut5.png";
+import ImgDesiccatedCoconut6 from "./DesiccatedCoconut/desiccated-coconut6.png";
+
+//VirginCoconutOil
+import ImgVirginCoconutOil1 from "./VirginCoconutOil/virgin-coconut-oil1.png";
+import ImgVirginCoconutOil2 from "./VirginCoconutOil/virgin-coconut-oil2.png";
+import ImgVirginCoconutOil3 from "./VirginCoconutOil/virgin-coconut-oil3.png";
+import ImgVirginCoconutOil4 from "./VirginCoconutOil/virgin-coconut-oil4.png";
+import ImgVirginCoconutOil5 from "./VirginCoconutOil/virgin-coconut-oil5.png";
+import ImgVirginCoconutOil6 from "./VirginCoconutOil/virgin-coconut-oil6.png";
+
+//Briquette
+import ImgBriquette1 from "./Briquette/briquette1.png";
+import ImgBriquette2 from "./Briquette/briquette2.png";
+import ImgBriquette3 from "./Briquette/briquette3.png";
+import ImgBriquette4 from "./Briquette/briquette4.png";
+import ImgBriquette5 from "./Briquette/briquette5.png";
+import ImgBriquette6 from "./Briquette/briquette6.png";
+
 export {
   jumbotron1,
   LogoWhite,
@@ -48,4 +72,22 @@ export {
   BannerProduct1,
   BannerProduct2,
   BannerProduct3,
+  ImgDesiccatedCoconut1,
+  ImgDesiccatedCoconut2,
+  ImgDesiccatedCoconut3,
+  ImgDesiccatedCoconut4,
+  ImgDesiccatedCoconut5,
+  ImgDesiccatedCoconut6,
+  ImgVirginCoconutOil1,
+  ImgVirginCoconutOil2,
+  ImgVirginCoconutOil3,
+  ImgVirginCoconutOil4,
+  ImgVirginCoconutOil5,
+  ImgVirginCoconutOil6,
+  ImgBriquette1,
+  ImgBriquette2,
+  ImgBriquette3,
+  ImgBriquette4,
+  ImgBriquette5,
+  ImgBriquette6,
 };
--- src/screens/ProductDetailView.js
+++ src/screens/ProductDetailView.js
@@ -4,15 +4,77 @@ import Spinner from "react-bootstrap/Spinner";
 import { Jumbotron } from "components/jumbotron";
 import productJson from "data/detailProduct.json";
 import { Base } from "components/base";
-import { BannerProduct1, BannerProduct2, BannerProduct3 } from "assets/images";
+import {
+  BannerProduct1,
+  BannerProduct2,
+  BannerProduct3,
+  ImgVirginCoconutOil1,
+  ImgVirginCoconutOil2,
+  ImgVirginCoconutOil3,
+  ImgVirginCoconutOil4,
+  ImgVirginCoconutOil5,
+  ImgVirginCoconutOil6,
+  ImgBriquette1,
+  ImgBriquette2,
+  ImgBriquette3,
+  ImgBriquette4,
+  ImgBriquette5,
+  ImgBriquette6,
+  ImgDesiccatedCoconut1,
+  ImgDesiccatedCoconut2,
+  ImgDesiccatedCoconut3,
+  ImgDesiccatedCoconut4,
+  ImgDesiccatedCoconut5,
+  ImgDesiccatedCoconut6,
+} from "assets/images";
+import styles from "./ProductDetailView.module.scss";
 
 function ProductDetailView() {
   const [data, setData] = useState(null);
+  const [idxImgSelected, setIdxImgSelected] = useState(0);
+  const [minIdxImgShow, setMinIdxImgShow] = useState(0);
+  const [maxIdxImgShow, setMaxIdxImgShow] = useState(3);
   const params = useParams();
   const { key } = params;
 
   const getData = useCallback(() => {
     const product = productJson.find((item) => (item.key = key));
+
+    switch (key) {
+      case "coconut-charcoal-briquette":
+        product.images = [
+          ImgBriquette1,
+          ImgBriquette2,
+          ImgBriquette3,
+          ImgBriquette4,
+          ImgBriquette5,
+          ImgBriquette6,
+        ];
+        break;
+      case "desiccated-coconut":
+        product.images = [
+          ImgDesiccatedCoconut1,
+          ImgDesiccatedCoconut2,
+          ImgDesiccatedCoconut3,
+          ImgDesiccatedCoconut4,
+          ImgDesiccatedCoconut5,
+          ImgDesiccatedCoconut6,
+        ];
+        break;
+      case "virgin-coconut-oil":
+        product.images = [
+          ImgVirginCoconutOil1,
+          ImgVirginCoconutOil2,
+          ImgVirginCoconutOil3,
+          ImgVirginCoconutOil4,
+          ImgVirginCoconutOil5,
+          ImgVirginCoconutOil6,
+        ];
+        break;
+      default:
+        break;
+    }
+
     setData(product);
   }, [key]);
 
@@ -36,11 +98,50 @@ function ProductDetailView() {
   return (
     <Base>
       {data ? (
-        <Jumbotron
-          image={handleImageBanner()}
-          h1={data.banner.h1}
-          h3={data.banner.h3}
-        />
+        <React.Fragment>
+          <Jumbotron
+            image={handleImageBanner()}
+            h1={data.banner.h1}
+            h3={data.banner.h3}
+          />
+          <div className={styles.containerContent}>
+            <div className={styles.ingDisplayWrapper}>
+              <img
+                src={data.images[idxImgSelected]}
+                className={styles.imgDisplay}
+                alt={""}
+              />
+              <div className={styles.imgCatalogueWrapper}>
+                {data.images
+                  .filter(
+                    (_, idx) => idx >= minIdxImgShow && idx <= maxIdxImgShow
+                  )
+                  .map((img, idx) => (
+                    <img
+                      key={idx.toString()}
+                      src={img}
+                      alt={""}
+                      onClick={() => setIdxImgSelected(idx)}
+                      className={styles.imgCatalogue}
+                    />
+                  ))}
+              </div>
+            </div>
+            <div style={{ flex: 1 }}>
+              <h2>{data.name}</h2>
+              <h4>{data.tagline}</h4>
+              {data.desc.map((desc, idx) => (
+                <p key={idx.toString()} style={{ width: 500 }}>
+                  {desc}
+                </p>
+              ))}
+            </div>
+          </div>
+          <div>
+            <button>Download Catalog</button>
+            <button>Contact Us</button>
+          </div>
+        </React.Fragment>
       ) : (
         <Spinner animation="grow" variant="success" />
       )}
